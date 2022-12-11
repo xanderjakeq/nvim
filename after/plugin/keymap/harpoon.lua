7819969c9532797f60d7c5d5697a2b690c73a626
xanderjakeq <xanderjakeq@pop-os.localdomain>
2022-12-11 11:43:45 -0500

    add harpoon bindings for terminal navigation

--- after/plugin/keymap/harpoon.lua
+++ after/plugin/keymap/harpoon.lua
@@ -4,9 +4,17 @@ local silent = { silent = true }
 
 nnoremap("<leader>ha", function() require("harpoon.mark").add_file() end, silent)
 nnoremap("<C-h>", function() require("harpoon.ui").toggle_quick_menu() end, silent)
-nnoremap("<leader>tc", function() require("harpoon.cmd-ui").toggle_quick_menu() end, silent)
 
 nnoremap("<C-n>", function() require("harpoon.ui").nav_file(1) end, silent)
 nnoremap("<C-e>", function() require("harpoon.ui").nav_file(2) end, silent)
 nnoremap("<C-i>", function() require("harpoon.ui").nav_file(3) end, silent)
 nnoremap("<C-o>", function() require("harpoon.ui").nav_file(4) end, silent)
+
+
+-- cmd-ui doesn't show anything
+-- nnoremap("<leader>th", function() require("harpoon.cmd-ui").toggle_quick_menu() end, silent)
+
+nnoremap("<C-t>n", function() require("harpoon.term").gotoTerminal(1) end, silent)
+nnoremap("<C-t>e", function() require("harpoon.term").gotoTerminal(2) end, silent)
+nnoremap("<C-t>i", function() require("harpoon.term").gotoTerminal(3) end, silent)
+nnoremap("<C-t>o", function() require("harpoon.term").gotoTerminal(4) end, silent)
