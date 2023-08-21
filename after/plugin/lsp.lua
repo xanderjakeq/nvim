b09584357ac5cc3822b666da74dcd207159ef7b1
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-08-21 02:40:56 -0400

    only one of tsserver and denols should run at a time

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,33 +4,36 @@ local lspconfig = require("lspconfig")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  --'sumneko_lua',
-  'rust_analyzer',
-  'pyright',
-  'svelte',
+    'tsserver',
+    --'sumneko_lua',
+    'rust_analyzer',
+    'pyright',
+    'svelte',
 })
 
-lspconfig.gdscript.setup{}
+lspconfig.gdscript.setup {}
+lspconfig.deno.setup {}
+lspconfig.tsserver.setup {}
+
 
 -- nvim-cmp setup
 local cmp = require('cmp')
 
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 local cmp_sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp', keyword_length = 3},
-    {name = 'cmp_tabnine' }, -- add tabnine
-    {name = 'buffer', keyword_length = 3},
-    {name = 'luasnip', keyword_length = 2},
+    { name = 'path' },
+    { name = 'nvim_lsp',   keyword_length = 3 },
+    { name = 'cmp_tabnine' }, -- add tabnine
+    { name = 'buffer',     keyword_length = 3 },
+    { name = 'luasnip',    keyword_length = 2 },
 }
 
 local source_mapping = {
@@ -61,7 +64,7 @@ lsp.setup_nvim_cmp({
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'ge', function() vim.diagnostic.open_float() end, opts)
 vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
 vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
@@ -70,28 +73,39 @@ vim.keymap.set('n', '<space>q', function() vim.diagnostic.setloclist() end, opts
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local _opts = {buffer = bufnr, remap = false}
+    local _opts = { buffer = bufnr, remap = false }
 
-  if client.name == "eslint" then
-      vim.cmd [[ LspStop eslint ]]
-      return
-  end
+    -- prevent stop tsserver on deno projects
+    if require("lspconfig").util.root_pattern("deno.json", "deno.jsonc")(vim.fn.getcwd()) then
+        if client.name == "tsserver" then
+            client.stop()
+            return
+        elseif client.name == "deno" then
+            client.stop()
+            return
+        end
+    end
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.keymap.set( 'n', 'gD', function() vim.lsp.buf.declaration() end, _opts)
-  vim.keymap.set( 'n', 'gd', function() vim.lsp.buf.definition() end, _opts)
-  vim.keymap.set( 'n', 'K', function() vim.lsp.buf.hover() end, _opts)
-  vim.keymap.set( 'n', 'gi', function() vim.lsp.buf.implementation() end, _opts)
-  vim.keymap.set( 'n', '<C-k>', function() vim.lsp.buf.signature_help() end, _opts)
-  vim.keymap.set( 'n', '<space>wa', function() vim.lsp.buf.add_workspace_folder() end, _opts)
-  vim.keymap.set( 'n', '<space>wr', function() vim.lsp.buf.remove_workspace_folder() end, _opts)
-  vim.keymap.set( 'n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, _opts)
-  vim.keymap.set( 'n', '<space>D', function() vim.lsp.buf.type_definition() end, _opts)
-  vim.keymap.set( 'n', '<space>rn', function() vim.lsp.buf.rename() end, _opts)
-  vim.keymap.set( 'n', '<space>ca', function() vim.lsp.buf.code_action() end, _opts)
-  vim.keymap.set( 'n', 'gr', function() vim.lsp.buf.references() end, _opts)
-  vim.keymap.set( 'n', '<leader><leader>p', function() vim.lsp.buf.format({async = true}) end, _opts)
+    if client.name == "eslint" then
+        vim.cmd [[ LspStop eslint ]]
+        return
+    end
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, _opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, _opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, _opts)
+    vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, _opts)
+    vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, _opts)
+    vim.keymap.set('n', '<space>wa', function() vim.lsp.buf.add_workspace_folder() end, _opts)
+    vim.keymap.set('n', '<space>wr', function() vim.lsp.buf.remove_workspace_folder() end, _opts)
+    vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, _opts)
+    vim.keymap.set('n', '<space>D', function() vim.lsp.buf.type_definition() end, _opts)
+    vim.keymap.set('n', '<space>rn', function() vim.lsp.buf.rename() end, _opts)
+    vim.keymap.set('n', '<space>ca', function() vim.lsp.buf.code_action() end, _opts)
+    vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, _opts)
+    vim.keymap.set('n', '<leader><leader>p', function() vim.lsp.buf.format({ async = true }) end, _opts)
 end
 
 lsp.on_attach(on_attach)
