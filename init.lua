45f2d4d26c87b1093550dfa75397446e12d51ef3
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2025-02-28 14:10:50 -0500

    limit completion suggestions

--- init.lua
+++ init.lua
@@ -94,6 +94,8 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.opt.pumheight = 5
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
