7589c5bb138cc02588a97faf8da2067d75482322
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2025-03-02 05:49:56 -0500

    add rust formatter

--- init.lua
+++ init.lua
@@ -580,6 +580,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        rust = { 'rustfmt', lsp_format = 'fallback' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
